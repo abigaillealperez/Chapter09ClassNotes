86779a95f51dd36963f585fdef2daf0264417d3b
abigaillealperez <lealperezabigail.alp@gmail.com>
2020-01-24 20:36:27 -0500

    Create Form1.Designer.cs

---
+++ Chapter09programClassDesign/Chapter09programClassDesign/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Chapter09programClassDesign
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEnterYear = new System.Windows.Forms.Label();
+            this.lblLeapYearResult = new System.Windows.Forms.Label();
+            this.lblEasterResult = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblEnterYear
+            // 
+            this.lblEnterYear.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblEnterYear.Location = new System.Drawing.Point(31, 21);
+            this.lblEnterYear.Name = "lblEnterYear";
+            this.lblEnterYear.Size = new System.Drawing.Size(95, 30);
+            this.lblEnterYear.TabIndex = 0;
+            this.lblEnterYear.Text = "Enter year:";
+            this.lblEnterYear.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblLeapYearResult
+            // 
+            this.lblLeapYearResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblLeapYearResult.Location = new System.Drawing.Point(31, 136);
+            this.lblLeapYearResult.Name = "lblLeapYearResult";
+            this.lblLeapYearResult.Size = new System.Drawing.Size(212, 23);
+            this.lblLeapYearResult.TabIndex = 1;
+            this.lblLeapYearResult.Click += new System.EventHandler(this.lblLeapYearResult_Click);
+            // 
+            // lblEasterResult
+            // 
+            this.lblEasterResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblEasterResult.Location = new System.Drawing.Point(31, 174);
+            this.lblEasterResult.Name = "lblEasterResult";
+            this.lblEasterResult.Size = new System.Drawing.Size(212, 23);
+            this.lblEasterResult.TabIndex = 2;
+            this.lblEasterResult.Click += new System.EventHandler(this.lblEasterResult_Click);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(31, 101);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 3;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(168, 101);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(143, 21);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(100, 20);
+            this.txtYear.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 259);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.lblEasterResult);
+            this.Controls.Add(this.lblLeapYearResult);
+            this.Controls.Add(this.lblEnterYear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEnterYear;
+        private System.Windows.Forms.Label lblLeapYearResult;
+        private System.Windows.Forms.Label lblEasterResult;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtYear;
+    }
+}
+
